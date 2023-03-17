369061cbedd2bc3dd382410a9236248b7fea433d
Kinga Palijewska <kinga.jakubowska09@gmail.com>
2023-03-17 23:53:38 +0100

    added currency board

---
+++ src/App/currencies.js
@@ -0,0 +1,17 @@
+export const currencies = [
+    {
+        short: "USD",
+        name: "Dolar amerykański",
+        rate: 4.3341,
+    },
+    {
+        short: "EUR",
+        name: "Euro",
+        rate: 4.7073,
+    },
+    {
+        short: "GBP",
+        name: "Funt brytyjski",
+        rate: 5.3443,
+    }
+]
