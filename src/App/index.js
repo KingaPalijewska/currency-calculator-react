52adfc29bb08fc2f0388d0a42058977ccdb4d913
Kinga Palijewska <kinga.jakubowska09@gmail.com>
2023-03-18 00:01:58 +0100

    added function calculateResult andobject setResult

---
+++ src/App/index.js
@@ -0,0 +1,18 @@
+import { useState } from 'react';
+
+function App() {
+
+    const [result, setresult] = useState();
+
+    const calculateResult = (currency, amount) => {
+        const rate = currencies
+            .find(({ short }) => short === currency)
+            .rate;
+
+        setResult({
+            sourceAmount: +amount,
+            targetAmount: amount / rate,
+            currency,
+        });
+    }
+}
