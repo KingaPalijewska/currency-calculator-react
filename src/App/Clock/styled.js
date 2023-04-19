14a9987744fbd99aa2d5c84b5032bf9eed25a8fd
Kinga Palijewska <kinga.jakubowska09@gmail.com>
2023-04-19 17:55:45 +0200

    adding styled components to the Clock  component

---
+++ src/App/Clock/styled.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+    text-align: right;
+    font-family: monospace;
+    color: ${({ theme }) => theme.color.boulder};
+
+`;
